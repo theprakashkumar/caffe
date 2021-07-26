d1e2f4f11b75f24ac0210adb1c22effa517b1a45
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-07-26 10:32:26 +0530

    feat: implemented wishlist button on product card

--- README.md
+++ README.md
@@ -20,3 +20,5 @@ Ecom
 -   Wrap all the context into one.
 
 -   When the cart and wishlist is empty and I insert a item to wishlist now when I move that item to cart it goes into cart but when I do back on the product page it still show ADD TO CART.
+
+-   Add which link item is active.
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,49 +1,142 @@
-import { Link } from 'react-router-dom';
-import './ProductCard.css';
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useState, useContext, useEffect } from "react";
+import { WishlistContext } from "../contexts/WishlistContext";
+import { AuthContext } from "../contexts/AuthContext";
+import "./ProductCard.css";
 
 const ProductCard = (props) => {
-    const {_id, name, image, price, ratings, mrp, discount} = props.details;
-    return(
+    const { _id, name, image, price, ratings, mrp, discount } = props.details;
+    const { isUserLogin, userId, token } = useContext(AuthContext);
+
+    const { state: wishlistState, dispatch: wishlistDispatch } =
+        useContext(WishlistContext);
+
+    const [isProductInWishlist, setIsProductInWishlist] = useState(false);
+
+    const inWishlist = (id) => {
+        const alreadyInWishlist = wishlistState.find((item) => item._id === id);
+        if (alreadyInWishlist) {
+            setIsProductInWishlist(true);
+        }
+    };
+
+    // add product to wishlist
+    const addToWishlist = async (id) => {
+        console.log("Adding to Wishlist");
+        try {
+            const response = await axios.post(
+                `/wishlist/${userId}`,
+                {
+                    _id: id,
+                },
+                {
+                    headers: {
+                        authorization: token,
+                    },
+                }
+            );
+            if (response.data.success) {
+                console.log("Added to Wishlist");
+                console.log(response.data);
+                wishlistDispatch({
+                    type: "SYNC_WISHLIST",
+                    payload: {
+                        product: response.data.updatedWishlist.wishlistItems,
+                    },
+                });
+                setIsProductInWishlist(true);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // remove product from the wishlist
+    const removeFromWishlist = async (id) => {
+        try {
+            const response = await axios.delete(`/wishlist/${userId}`, {
+                headers: {
+                    authorization: token,
+                },
+                data: {
+                    _id,
+                },
+            });
+            if (response.data.success) {
+                wishlistDispatch({
+                    type: "SYNC_WISHLIST",
+                    payload: {
+                        product: response.data.wishlist.wishlistItems,
+                    },
+                });
+            }
+            setIsProductInWishlist(false);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const clickHandler = () => {
+        !isProductInWishlist ? addToWishlist(_id) : removeFromWishlist(_id);
+    };
+
+    useEffect(() => {
+        inWishlist(_id);
+    }, []);
+
+    useEffect(() => {
+        inWishlist(_id);
+    }, [wishlistState]);
+
+    return (
         <div class="card-product">
-                <div class="card-product__image__wrapper">
-                    <img src={ image } alt="product" class="card-product__image" />
-                    <button class="card-product__icon"
-                    >
-                        <span class="material-icons md-36">
-                            favorite_border
-                        </span>
-                    </button>
-                </div>
-
-                <div class="card-product__name">{ name }</div>
-
-                <div class="card-product__description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum accusamus, quo veniam at ipsum
-                </div>
-
-                <div class="card-product__rating__wrapper">
-                    <div class="card-product__rating__icon__wrapper">
-                        <span class="card-product__rating__icon__text">{ ratings }</span>
-                        <span class="material-icons-outlined card-product__rating__icon">
-                            star
-                        </span>
-                    </div>
-                    <span class="card-product__rating__reviews">342 Ratings & 29 Reviews</span>
-                </div>
-
-                <div class="card-product__price__wrapper">
-                    <div class="card-product__price">{'\u20B9'} {price}</div>
-                    <strike class="card-product__price__crossed ml-2">{'\u20B9'} {mrp} </strike>
-                    <div class="card-product__price__discount ml-2">{discount} off</div>
-                </div>
-
-                <Link to={`/product/${_id}`}>
-                    <button class="btn mt-1">SHOP NOW</button>
-                </Link>
-
+            <div class="card-product__image__wrapper">
+                <img src={image} alt="product" class="card-product__image" />
+                <button class="card-product__icon" onClick={clickHandler}>
+                    {isProductInWishlist ? "wished" : "wish"}
+                    {/* <span class="material-icons md-36">favorite_border</span> */}
+                </button>
             </div>
 
-    )
-}
+            <div class="card-product__name">{name}</div>
+
+            <div class="card-product__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+                accusamus, quo veniam at ipsum
+            </div>
+
+            <div class="card-product__rating__wrapper">
+                <div class="card-product__rating__icon__wrapper">
+                    <span class="card-product__rating__icon__text">
+                        {ratings}
+                    </span>
+                    <span class="material-icons-outlined card-product__rating__icon">
+                        star
+                    </span>
+                </div>
+                <span class="card-product__rating__reviews">
+                    342 Ratings & 29 Reviews
+                </span>
+            </div>
+
+            <div class="card-product__price__wrapper">
+                <div class="card-product__price">
+                    {"\u20B9"} {price}
+                </div>
+                <strike class="card-product__price__crossed ml-2">
+                    {"\u20B9"} {mrp}{" "}
+                </strike>
+                <div class="card-product__price__discount ml-2">
+                    {discount} off
+                </div>
+            </div>
+
+            <Link to={`/product/${_id}`}>
+                <button class="btn mt-1">SHOP NOW</button>
+            </Link>
+        </div>
+    );
+};
 
 export default ProductCard;
