940f83031b47106eb018632f3406255297ba44b1
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-08-01 12:19:02 +0530

    style: styled empty cart component

--- src/components/EmptyCart.js
+++ src/components/EmptyCart.js
@@ -3,14 +3,30 @@ import { Link } from "react-router-dom";
 import Cart from "../assets/add_shopping_cart_black_48dp.svg";
 const EmptyCart = () => {
     return (
-        <div>
-            <h1>Cart</h1>
-            <img src={Cart} alt="React Logo" />
-            <h2>I am empty :(</h2>
-            <h4>Your Cart is Empty</h4>
-            <Link to="/products">Go to Catalog</Link>
+        <div className="empty-cart flex flex-dir-cl flex-align-center">
+            <div className="heading--h5">Cart</div>
+            <img
+                className="empty-cart__image mt-2"
+                src={Cart}
+                alt="Empty Cart Logo"
+            />
+            <div className="heading--h6">I am empty :(</div>
+            <div>Your Cart is Empty</div>
+            <Link className="btn btn--link" to="/products">
+                Go to Catalog
+            </Link>
         </div>
     );
 };
 
 export default EmptyCart;
+
+{
+    /* <div className="empty-wishlist flex flex-dir-cl flex-align-center">
+<div className="heading--h5">Wishlist</div>
+<img  className="empty-wishlist__image" src={emptyWishlist} alt="Empty Wishlist Logo" />
+<div className="heading--h6">I am empty :(</div>
+<div>Your Wishlist is Empty</div>
+<Link className="btn btn--link" to="/products">Go to Catalog</Link>
+</div> */
+}
--- src/components/EmptyWishlist.js
+++ src/components/EmptyWishlist.js
@@ -5,10 +5,16 @@ const EmptyWishlist = () => {
     return (
         <div className="empty-wishlist flex flex-dir-cl flex-align-center">
             <div className="heading--h5">Wishlist</div>
-            <img  className="empty-wishlist__image" src={emptyWishlist} alt="Empty Wishlist Logo" />
+            <img
+                className="empty-wishlist__image"
+                src={emptyWishlist}
+                alt="Empty Wishlist Logo"
+            />
             <div className="heading--h6">I am empty :(</div>
             <div>Your Wishlist is Empty</div>
-            <Link className="btn btn--link" to="/products">Go to Catalog</Link>
+            <Link className="btn btn--link" to="/products">
+                Go to Catalog
+            </Link>
         </div>
     );
 };
