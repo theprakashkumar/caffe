bc69c8902cb981f96965ca283ea5073545eec821
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-08-01 11:57:01 +0530

    style: styled EmptyWishlist component

--- src/components/EmptyWishlist.js
+++ src/components/EmptyWishlist.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import emptyWishlist from "../assets/bookmark_add_black_48dp.svg";
 const EmptyWishlist = () => {
     return (
-        <div>
-            <h1>Wishlist</h1>
-            <img src={emptyWishlist} alt="React Logo" />
-            <h2>I am empty :(</h2>
-            <h4>Your Wishlist is Empty</h4>
-            <Link to="/products">Go to Catalog</Link>
+        <div className="empty-wishlist flex flex-dir-cl flex-align-center">
+            <div className="heading--h5">Wishlist</div>
+            <img  className="empty-wishlist__image" src={emptyWishlist} alt="Empty Wishlist Logo" />
+            <div className="heading--h6">I am empty :(</div>
+            <div>Your Wishlist is Empty</div>
+            <Link className="btn btn--link" to="/products">Go to Catalog</Link>
         </div>
     );
 };
